4a0eb7994258d14582366f74228dc3ce413f5424
ronald <rordonez@softecdigital.com>
2020-02-25 20:18:12 +0800

    initial commit all files

---
+++ includes/head-tag.php
@@ -0,0 +1,22 @@
+<title><?php print $PAGE_TITLE;?></title>
+
+<?php if ($CURRENT_PAGE == "Index") { ?>
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" /> 
+<?php } ?>
+
+<!-- MAIN STYLESHEET -->
+<link rel="stylesheet" type="text/css" href="./assets/css/main-stylesheet.css">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+<!-- <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"> -->
+
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+<!-- Main javascript -->
+<script src="./assets/js/main.js"></script>
